1aac647c7354dc830322ae91db5269324a9b58bb
Inma <inmanewmail@gmail.com>
2019-10-17 11:24:54 +0200

    bug in route /create-report solved

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -28,8 +28,7 @@ router.get('/create-report', (req, res, next) => {
     Employee.findOne({name})
     .then(theEmployee => {
       employee_id = theEmployee._id
-
-      MonthlyReport.findOne({employee_id, year, month})
+      return MonthlyReport.findOne({employee_id, year, month})  })
       .then(theReport => {
         if (theReport !== null) {
           res.render('reports/report', {theReport, name})
@@ -39,7 +38,7 @@ router.get('/create-report', (req, res, next) => {
           .then(theReport => res.render('reports/report', {theReport, name}))
         }
       })    
-    })
+    
     .catch(err => console.log('error getting the employee report ', err))
   }
     else {res.render('reports/report', {message: "You must set employee name, year and month"})}
@@ -57,32 +56,26 @@ router.post('/create-report', (req,res, next) => {
   let employee_id = ''
 
   // first we check if the employee and date are selected in report.hbs and the query isn't empty
-  if(req.query.name === '' || req.query.year === '' || req.query.month === '') 
+  if(name === '' || year === '' || month === '') 
   {res.render('reports/report', {message: "You must select first an employee and a date (year and month)"})} 
   
   // secondly we check if the form fields are correctly completed
   if(day == 'Select' || transportation == null || parking == null)
-     {res.render('reports/report', {message: "You must select an option for every field"})}
+     {res.render('reports/report', {message: "You must select an option for every field"})
+      return}
 
   // then we fetch de employee_id   
   Employee.findOne({name})
-  .then(theEmployee => {
-    employee_id = theEmployee._id
-  })  
+  .then(theEmployee => employee_id = theEmployee._id)  
   
   // now we update de dailies field in DB for the report we are working with and the total, and send the info in report.hbs
   MonthlyReport.findOneAndUpdate({year, month}, {$push: {dailies: {day, meal, transportation, parking}}}, {new:true})
   .populate(employee_id)
   .then(theReport => {
     let total = getTotalfromDB(theReport.dailies)
-
-    MonthlyReport.findOneAndUpdate({year, month}, {total}, {new:true})
-    .then(theReport => {
-      res.render('reports/report', {theReport, name})
-    })
-  })
+    return MonthlyReport.findOneAndUpdate({year, month}, {total}, {new:true})   })
+  .then(theReport => res.render('reports/report', {theReport, name}))
   .catch(err => console.log('error checking existing report and creating new ', err))
-
 })
 
 
@@ -111,22 +104,21 @@ router.post('/update' , (req,res,next) => {
 
   const {day, meal, transportation, parking} = req.body
   const {cId, cDay, cMeal, cTransportation, cParking} = req.query
+  let theReport = {}
 
   MonthlyReport.findOneAndUpdate({"dailies": {_id: cId, day: cDay, meal: cMeal, transportation: cTransportation, parking: cParking}}, 
                                   {$set: {"dailies.$": {_id: cId, day, meal, transportation, parking}}}, {new:true})
   .then(theReport => {
-
-    let total = getTotalfromDB(theReport.dailies)
-    MonthlyReport.findOneAndUpdate({_id: theReport._id}, {total}, {new:true})
-    .then(theReport => {
-      Employee.findById(theReport.employee_id)
-      .then(theEmployee => {
-        let name = theEmployee.name
-        res.render('reports/report', {theReport, name})
-      })
+      let total = getTotalfromDB(theReport.dailies)
+      return MonthlyReport.findOneAndUpdate({_id: theReport._id}, {total}, {new:true}) })
+  .then(report => {
+    theReport = report
+    return Employee.findById(theReport.employee_id)})
+  .then(theEmployee => {
+      let name = theEmployee.name
+      res.render('reports/report', {theReport, name})
     })
-    .catch(err => console.log('error updating the daily report', err))
-  })
+  .catch(err => console.log('error updating the daily report', err))
 })
 
 
